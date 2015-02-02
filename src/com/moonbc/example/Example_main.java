ffc3e49d8148e7ae3a8ecd58a99c481c9f7f3b08
moonbc <lightbc@gmail.com>
2015-02-02 17:23:02 +0900

    test22

--- src/com/moonbc/example/Example_main.java
+++ src/com/moonbc/example/Example_main.java
@@ -6,6 +6,8 @@ import com.moonbc.example.simuduck.Duck;
 import com.moonbc.example.simuduck.FlyBehavior;
 import com.moonbc.example.simuduck.FlyNoWay;
 import com.moonbc.example.simuduck.MallardDuck;
+import com.moonbc.example.weatherstation.CurrentConditionsDisplay;
+import com.moonbc.example.weatherstation.WeatherData;
 
 public class Example_main {
 
@@ -14,12 +16,23 @@ public class Example_main {
 //        Main_Orthogonality main_Orthogonality= new Main_Orthogonality();
 //        main_Orthogonality.excute();
     	
-    	Duck mallardDuck = new MallardDuck();
-    	
-    	mallardDuck.performFly();
-    	mallardDuck.setFlyBehavior(new FlyNoWay());
-    	
-    	mallardDuck.performFly();
+//    	Duck mallardDuck = new MallardDuck();
+//    	
+//    	mallardDuck.performFly();
+//    	mallardDuck.setFlyBehavior(new FlyNoWay());
+//    	
+//    	mallardDuck.performFly();
+        
+        WeatherData weatherData = new WeatherData();
+        
+        CurrentConditionsDisplay currentDisplay = 
+            new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
         
     }
 }
---
+++ src/com/moonbc/example/weatherstation/CurrentConditionsDisplay.java
@@ -0,0 +1,14 @@
+package com.moonbc.example.weatherstation;
+
+public class CurrentConditionsDisplay {
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public CurrentConditionsDisplay(Subject weatherData) {
+        // TODO Auto-generated constructor stub
+    }
+
+    
+}
